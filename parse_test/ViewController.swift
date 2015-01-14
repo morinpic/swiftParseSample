e805f27c4224eee33abd2893ed242e70f85f6327
kenta-mori <kenta_mori@waku-2.com>
2015-01-14 20:16:47 +0900

    Initial Commit

---
+++ parse_test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  parse_test
+//
+//  Created by 森健太 on 2015/01/14.
+//  Copyright (c) 2015年 stoop. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
