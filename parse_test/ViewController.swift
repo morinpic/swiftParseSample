23c2b9f1a3fd46017cb01835568edcd728fc702a
kenta-mori <kenta_mori@waku-2.com>
2015-01-16 16:51:15 +0900

    first commit

--- parse_test/ViewController.swift
+++ parse_test/ViewController.swift
@@ -13,13 +13,22 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        var testObject:PFObject = PFObject(className: "TestObject")
+        testObject["foo"] = "bar"
+        testObject.setObject("user1", forKey: "user")
+        
+        // 上記コードの説明は以下。
+        // var 適当な変数:PFObject = PFObject(className: "テーブル名")
+        // testObject["カラム名"] = "テスト用に突っ込むデータ"
+        // testObject.setObject("テスト用に突っ込むデータ", forKey: "カラム名")
+        
+        testObject.saveInBackground()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
-
